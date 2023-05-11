231a553622e7c3691108bf432e44db0bf11d960f
waletaiwo <dewalebabz@gmail.com>
2023-05-11 14:29:00 +0100

    add variables

--- main.tf
+++ main.tf
@@ -5,17 +5,30 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "Infa-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.vpc_azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
-  enable_nat_gateway = true
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  tags = var.tags
 }
+
+#TODO: add ec2 module 
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  
+  
+}
+
+# # add rds module 
+# module "rds" {
+  
+# }
+
+# #add load balancer 
+# module "load_balancer" {
+  
+# }
