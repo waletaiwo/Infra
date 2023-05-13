8a151d6f45ea435c68a43cac3808d8ab371e1d20
waletaiwo <dewalebabz@gmail.com>
2023-05-14 00:29:37 +0100

    add ec2 module

--- main.tf
+++ main.tf
@@ -4,35 +4,42 @@ provider "aws" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+  name   = var.vpc_name
+  cidr   = var.vpc_cidr
 
   azs             = var.vpc_azs
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
-
+  enable_ipv6     = true
 
   tags = var.tags
 }
 
+# module "security-group" {
+#   source            = "terraform-aws-modules/security-group/aws"
+#   security_group_id = module.vpc.default_security_group_id
+#   vpc_id            = module.vpc
+# }
+
 #TODO: add ec2 module 
 module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  
-  name = "single-instance"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  # subnet_id              = module.vpc.public_subnets
+  name                        = "single-instance"
+  instance_type               = "t2.micro"
+  ami                         = "ami-0889a44b331db0194"
+  availability_zone           = module.vpc.azs[0]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  tags                        = var.tags
 }
 
 # # add rds module 
 # module "rds" {
-  
+
 # }
 
 # #add load balancer 
 # module "load_balancer" {
-  
+
 # }
--- variables.tf
+++ variables.tf
@@ -1,43 +1,43 @@
 variable "vpc_name" {
-    description = "Name for vpc"
-    type = string
-    default = "Infra-vpc"
+  description = "Name for vpc"
+  type        = string
+  default     = "Infra-vpc"
 }
 
 variable "vpc_cidr" {
-    description = "Vpc cidr range"
-    type = string
-    default = "10.0.0.0/16"
-  
+  description = "Vpc cidr range"
+  type        = string
+  default     = "10.0.0.0/16"
+
 }
 
 variable "vpc_azs" {
-    description = "List of az for vpc"
-    type = list(string)
-    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  
+  description = "List of az for vpc"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
 }
 
 variable "vpc_private_subnets" {
-    description = "vpc private subnets"
-    type = list(string)
-    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  
+  description = "vpc private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
 }
 
 variable "vpc_public_subnets" {
-    description = "vpc public subnets"
-    type = list(string)
-    default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  
+  description = "vpc public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
 }
 
 variable "tags" {
-    description = "tags for project"
-    type = map(string)
-    default = {
-        Terraform = "true"
-        Environment = "dev"
-    }
-  
+  description = "tags for project"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
 }
